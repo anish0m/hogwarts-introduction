d99086d4b19fad0a0a1e70dff29e8cc55233fe53
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-02-25 14:42:38 +0600

    Starting the react-app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/logo-hogwarts.png" alt="Stylized atom" />
+        <h1>Hogwarts</h1>
+        <p>
+          <i>Draco dormiens nunquam titillandus</i>
+        </p>
+      </header>
+      <main>
+        <h2>Time to get sorted!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
