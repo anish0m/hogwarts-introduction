6261da04f1f0b6f049557f1018206db07df48a36
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-03-05 13:23:20 +0600

    Remove Bootstrap CSS import from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import ReactDOM from "react-dom/client";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import App from "./App.jsx";
 import "./index.css";
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
