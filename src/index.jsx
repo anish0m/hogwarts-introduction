5b2f1b3478b984310894294e90a429ad7d781a97
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-03-08 13:35:02 +0600

    Refactor index.jsx to use React.createElement for rendering App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -6,4 +7,4 @@ import "./index.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
